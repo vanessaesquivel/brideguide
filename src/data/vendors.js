194ce65f79897c5784db96872389bc738029ba0f
vanessaesquivel <34462552+vanessaesquivel@users.noreply.github.com>
2018-04-17 21:07:58 -0400

    Change vendor data file structure and add sample categories

--- src/data/vendors.js
+++ src/data/vendors.js
@@ -1,6 +1,6 @@
 
-const sample_vendors = [{
-    category: "Florist",
+const VENDOR_DATA = [{
+    category_id: 1,
     name: "Ever After Floral",
     short_description: "Local artist/entrepreneur Rachel Stevenson has made wedding and event floral design her life's passion. She has been a floral designer since 2004, first at a floral shop in Lancaster County and then branching out on her own business venture in 2008 doing exclusive wedding designs from her floral studio in Myerstown.",
     description: "Local artist/entrepreneur Rachel Stevenson has made wedding and event floral design her life's passion. She has been a floral designer since 2004, first at a floral shop in Lancaster County and then branching out on her own business venture in 2008 doing exclusive wedding designs from her floral studio in Myerstown. She has received training not only from her former floral position but also advanced training specifically in wedding design from instructors at the Longwood Gardens Design School. It is her goal to create works of art inspired by your personality and ideas as well as work within your wedding floral budget.",
@@ -16,7 +16,7 @@ const sample_vendors = [{
     }
   },
   {
-    category: "Decor",
+    category_id: 2,
     name: "Unearthead Rentals",
     short_description: "We conceptualized the idea of Unearthed Vintage in 2011, bringing our ideas to life in January of 2012 as South Florida’s first vintage rental company, leading the way in the event industry with our carefully curated collection.",
     description: "We conceptualized the idea of Unearthed Vintage in 2011, bringing our ideas to life in January of 2012 as South Florida’s first vintage rental company, leading the way in the event industry with our carefully curated collection. With a passion to remain the largest vintage rental company in the southeast, we are dedicated to continuously evolving by incorporating new trends with timeless classics. We believe in quality, both in the rentals we offer and the service we provide, building meaningful relationships with everyone we come in contact with. We exist to create memorable experiences through our unique style and innovation.",
@@ -33,7 +33,7 @@ const sample_vendors = [{
   },
 
   {
-    category: "Venue",
+    category_id: 3,
     name: "The Bath Club",
     short_description: "The Bath Club is a hidden gem located on the beach in the beautiful City of Miami Beach and is is the ideal setting for a romantic and memorable wedding.",
     description: "The Bath Club is a hidden gem located on the beach in the beautiful City of Miami Beach and is is the ideal setting for a romantic and memorable wedding. With over 26,000 square feet of event space, your guests will experience a different era as they enter our unique and charismatic rooms. From a simple yet distinctive cocktail to an 8-course sit-down meal, The Bath Club offers the ideal rental venue for an unforgettable event.",
@@ -50,7 +50,7 @@ const sample_vendors = [{
   },
 
   {
-    category: "Photography",
+    category_id: 4,
     name: "Christian Arevalo",
     short_description: "My camera is a tool to freeze moments in time. Moments shared  with your family, friends, or even strangers. These are moments we can never get back, and the only way to relive them is to be able to see them in front of you.",
     description: "My camera is a tool to freeze moments in time. Moments shared  with your family, friends, or even strangers. These are moments we can never get back, and the only way to relive them is to be able to see them in front of you, printed on paper or seen on a screen.  What makes me tick is having the ability to supply that frozen moment to everyone I can. If you can relive a moment through a photograph, what else can you ask for?",
@@ -67,7 +67,7 @@ const sample_vendors = [{
   },
 
   {
-    category: "Catering",
+    category_id: 5,
     name: "Aaron's Catering",
     short_description: "Aaron’s Catering is a food driven, farm to table, catering company for over 12 years providing innovative and out of the box presentations with a twist. ",
     description: "We focus on setting the trends by using innovation, tight relationships with local farmers, technology, culinary alliances and being heavily involved with the industry. Chef Brian Aaron brings food, art and science together to create an unbelievable experience along with an impeccable service, out of the box presentations and eye-candy staff.",
@@ -84,7 +84,7 @@ const sample_vendors = [{
   },
 
   {
-    category: "Music",
+    category_id: 6,
     name: "Biscayne String Quartet",
     short_description: "The Biscayne String Quartet is one of the most sought after Miami based ensembles for wedding ceremony music, corporate events, and parties.  ",
     description: "The Biscayne String Quartet is one of the most sought after Miami based ensembles for wedding ceremony music, corporate events, and parties.  The group performs both classical music as well as original arrangements of popular non-classical songs, bringing a level of artistry and professionalism that your guests are sure to notice.",
@@ -101,4 +101,4 @@ const sample_vendors = [{
   }
 ]
 
-export default sample_vendors;
+export default VENDOR_DATA;
