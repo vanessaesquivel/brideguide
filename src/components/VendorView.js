d965df060da4d36ef68e4c791c25668230c62069
vanessaesquivel <34462552+vanessaesquivel@users.noreply.github.com>
2018-04-17 20:48:24 -0400

    Add vendows sample file and update VendorView component

--- src/components/VendorView.js
+++ src/components/VendorView.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+
 class VendorView extends Component {
   render() {
+    const vendor = this.props.vendor;
+    const voucher = vendor.voucher;
+
     return (
       <div className="container">
         <div className="row">
           <div className="col">
-            <h2 className="mb-4">{this.props.voucherTitle}</h2>
+            <h2 className="mb-4">{voucher.title}</h2>
             <h3 className="text-muted mb-4">
-              {this.props.vendorName}
+              {vendor.name}
               <span class="fa fa-star text-warning ml-3"></span>
               <span class="fa fa-star text-warning"></span>
               <span class="fa fa-star text-warning"></span>
@@ -26,7 +30,7 @@ class VendorView extends Component {
                 Description
               </div>
               <div className="card-body">
-                <p className="card-text">{this.props.vendorDescription}</p>
+                <p className="card-text">{vendor.description}</p>
               </div>
             </div>
             <div className="card mb-4">
@@ -35,10 +39,10 @@ class VendorView extends Component {
               </div>
               <div className="card-body">
                 <address>
-                  <strong>{this.props.vendorName}</strong><br/>
-                  1355 Market St, Suite 900
-                  San Francisco, CA 94103<br/>
-                  <abbr title="Phone">P:</abbr> (123) 456-7890
+                  <strong>{vendor.name}</strong><br/>
+                {vendor.contact.address}<br/>
+                <abbr title="Phone">P:</abbr> {vendor.contact.phone}<br/>
+              <abbr title="Email">E:</abbr> {vendor.contact.email}
                 </address>
               </div>
             </div>
@@ -50,18 +54,18 @@ class VendorView extends Component {
                   <h6 className="my-0">Voucher Value</h6>
                   <small className="text-muted"></small>
                 </div>
-                <span className="text-muted">${this.props.voucherValue}</span>
+                <span className="text-muted">${voucher.value}</span>
               </li>
               <li className="list-group-item d-flex justify-content-between bg-light">
                 <div className="text-success">
                   <h6 className="my-0">BrideGuide discount</h6>
                   <small></small>
                 </div>
-                <span className="text-success">-${this.props.voucherDiscount}</span>
+                <span className="text-success">-${voucher.value - voucher.price}</span>
               </li>
               <li className="list-group-item d-flex justify-content-between">
                 <span>Total (USD)</span>
-                <strong>${this.props.voucherPrice}</strong>
+              <strong>${voucher.price}</strong>
               </li>
               <li className="list-group-item d-flex justify-content-between"><button type="button" className="btn btn-primary btn-lg btn-block">Purchase</button></li>
             </ul>
