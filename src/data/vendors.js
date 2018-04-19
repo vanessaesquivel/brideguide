3add1d7f053d869340841329b3cfc06f86235af6
vanessaesquivel <34462552+vanessaesquivel@users.noreply.github.com>
2018-04-18 20:26:25 -0400

    updated vendors data

--- src/data/vendors.js
+++ src/data/vendors.js
@@ -1,5 +1,6 @@
 
 const VENDOR_DATA = [{
+
     category_id: 1,
     name: "Ever After Floral",
     short_description: "Local artist/entrepreneur Rachel Stevenson has made wedding and event floral design her life's passion. She has been a floral designer since 2004, first at a floral shop in Lancaster County and then branching out on her own business venture in 2008 doing exclusive wedding designs from her floral studio in Myerstown.",
@@ -16,6 +17,55 @@ const VENDOR_DATA = [{
     }
   },
   {
+    category_id: 1,
+    name: "Bayfront Floral",
+    short_description: "Here at Bayfront Floral, we believe that on your special day, you deserve sensational floral designs that reflect you and your unique personality. ",
+    description: "Weddings are the heart of our business, and we feel honored to work with you in helping you transform your dreams into a reality. We aim to design signature floral arrangements that perfectly compliment all aspects of your wedding from your gown to your venue.",
+    contact: {
+      address: "100 Chopin Plaza, Miami, FL 33131",
+      phone: "(305)-375-0300",
+      email: "info@bayfrontfloral.com"
+    },
+    voucher: {
+      title: "Flowers Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 1,
+    name: "Tria's Flowers",
+    short_description: "Trias Flowers has been recognized and awarded #1 in City Search Miami and Coral Gables Living for five consecutive years and "Top 200" florists in the country for the last five years by Teleflora. ",
+    description: "Trias Flowers & Events is a full service floral wedding and event planning company. A fourth generation family-owned business, it is one of the highest regarded florists in South Florida. With over 100 years of experience, Trias Flowers & Events has a reputation for award-winning flowers, memorable weddings & events with warm, friendly service.",
+    contact: {
+      address: "6520 Bird Road Miami, FL 33155",
+      phone: "(305)-749-0314 ",
+      email: "info@triasevents.com"
+    },
+    voucher: {
+      title: "Flowers Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 1,
+    name: "Anthology Floristry & Event Design",
+    short_description: "Anthology Co. is a floral studio and styling boutique for social and corporate events. Behind the company stands a strong philosophy to render the beauty of nature, admiration to artful design and history while mastering the art of setting the table.",
+    description: "Anthology Co. is a floral studio and styling boutique for social and corporate events. Behind the company stands a strong philosophy to render the beauty of nature, admiration to artful design and history while mastering the art of setting the table. We are passionate about design, from the smallest detail to the grandest scheme, and we believe that styling flowers & events is an exciting, shared creative process with each client.",
+    contact: {
+      address: "6951 NW 16th St, Miami, FL 33126",
+      phone: "(305)-988-6612",
+      email: "info@anthologyfloristry.com"
+    },
+    voucher: {
+      title: "Flowers Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+
+  {
     category_id: 2,
     name: "Unearthead Rentals",
     short_description: "We conceptualized the idea of Unearthed Vintage in 2011, bringing our ideas to life in January of 2012 as South Florida’s first vintage rental company, leading the way in the event industry with our carefully curated collection.",
@@ -24,6 +74,55 @@ const VENDOR_DATA = [{
       address: "6951 nw 16th street, miami, Fl, 33126",
       phone: "(305)987-6946",
       email: "info@unearthedrentals.com"
+    },
+    voucher: {
+      title: "Decor Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+
+  {
+    category_id: 2,
+    name: "Event Decor South Florida",
+    short_description: "Event Decor South Florida was established by combining experienced lighting and draping professionals. With the combined experience of both groups of professionals we have one of the fastest turn key operations in the event industry, largest inventory in flame retardant fabrics and lighting fixtures.",
+    description: "Event Decor South Florida has multiple vehicles and a full staff of trained and experience event specialist. To ensure the best quality control we go through a rigorous training program for safety and proper installs and dismantles. We take a lot of pride of having the right licenses , insurance , and workers compensation. All of our drapes are flame certified which has met the US Flame Retardant Regulations. ",
+    contact: {
+      address: "6951 nw 16th street, miami, Fl, 33126",
+      phone: "(305)987-6946",
+      email: "info@unearthedrentals.com"
+    },
+    voucher: {
+      title: "Decor Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 2,
+    name: "",
+    short_description: ".",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Decor Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 2,
+    name: "",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
     },
     voucher: {
       title: "Decor Voucher",
@@ -48,6 +147,54 @@ const VENDOR_DATA = [{
       price: 750
     }
   },
+  {
+    category_id: 3,
+    name: "The Bath Club",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Venue Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 3,
+    name: "The Bath Club",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Venue Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
+  {
+    category_id: 3,
+    name: "The Bath Club",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Venue Voucher",
+      value: 1000,
+      price: 750
+    }
+  },
 
   {
     category_id: 4,
@@ -58,6 +205,54 @@ const VENDOR_DATA = [{
       address: "191 SW 12th Street, Miami, Fl, 33130",
       phone: "(786)-614-9908",
       email: "info@chrstianarevalo.com"
+    },
+    voucher: {
+      title: "Photography Voucher",
+      value: 0,
+      price: 0
+    }
+  },
+  {
+    category_id: 4,
+    name: "",
+    short_description: ".",
+    description: "",
+    contact: {
+      address: ,
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Photography Voucher",
+      value: 0,
+      price: 0
+    }
+  },
+  {
+    category_id: 4,
+    name: "",
+    short_description: ".",
+    description: "",
+    contact: {
+      address: ,
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "Photography Voucher",
+      value: 0,
+      price: 0
+    }
+  },
+  {
+    category_id: 4,
+    name: "",
+    short_description: ".",
+    description: "",
+    contact: {
+      address: ,
+      phone: "",
+      email: ""
     },
     voucher: {
       title: "Photography Voucher",
@@ -82,6 +277,54 @@ const VENDOR_DATA = [{
       price: 0
     }
   },
+  {
+    category_id: 5,
+    name: "",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "",
+      value: 0,
+      price: 0
+    }
+  },
+  {
+    category_id: 5,
+    name: "",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "",
+      value: 0,
+      price: 0
+    }
+  },
+  {
+    category_id: 5,
+    name: "",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
+    },
+    voucher: {
+      title: "",
+      value: 0,
+      price: 0
+    }
+  },
 
   {
     category_id: 6,
@@ -92,6 +335,22 @@ const VENDOR_DATA = [{
       address: "191 SW 12th Street,Miami,Fl,33130",
       phone: "(786)614-9908",
       email: "info@biscaynequartet.com"
+    },
+    voucher: {
+      title: "",
+      value: 0,
+      price: 0
+    }
+  }
+  {
+    category_id: 6,
+    name: "",
+    short_description: "",
+    description: "",
+    contact: {
+      address: "",
+      phone: "",
+      email: ""
     },
     voucher: {
       title: "",
