88744002cc12a68d79118847410d1fae8cd49f23
Gustavo Maggi <gemaggi@gmail.com>
2018-04-19 20:54:32 -0400

    Added images to categoryView

--- src/data/vendors.js
+++ src/data/vendors.js
@@ -5,7 +5,7 @@ const VENDOR_DATA = [{
     name: "Ever After Floral",
     short_description: "Local artist/entrepreneur Rachel Stevenson has made wedding and event floral design her life's passion. She has been a floral designer since 2004, first at a floral shop in Lancaster County and then branching out on her own business venture in 2008 doing exclusive wedding designs from her floral studio in Myerstown.",
     description: "Local artist/entrepreneur Rachel Stevenson has made wedding and event floral design her life's passion. She has been a floral designer since 2004, first at a floral shop in Lancaster County and then branching out on her own business venture in 2008 doing exclusive wedding designs from her floral studio in Myerstown. She has received training not only from her former floral position but also advanced training specifically in wedding design from instructors at the Longwood Gardens Design School. It is her goal to create works of art inspired by your personality and ideas as well as work within your wedding floral budget.",
-    image_url: "https://static1.squarespace.com/static/547131aae4b0d1dec986cd9e/59498698d482e9276513b790/5949875b1b631b3571bf93e6/1497992664746/20170523-amy-lauren-creative-process-24.JPG",
+    image_url: "Florist_1.jpg",
     contact: {
       address: "Carpenter Ave, Myerstown, PA 17067, USA",
       phone: "(717) 813-1768",
@@ -23,6 +23,7 @@ const VENDOR_DATA = [{
     name: "Bayfront Floral",
     short_description: "Here at Bayfront Floral, we believe that on your special day, you deserve sensational floral designs that reflect you and your unique personality. ",
     description: "Weddings are the heart of our business, and we feel honored to work with you in helping you transform your dreams into a reality. We aim to design signature floral arrangements that perfectly compliment all aspects of your wedding from your gown to your venue.",
+    image_url: "Florist_2.jpg",
     contact: {
       address: "100 Chopin Plaza, Miami, FL 33131",
       phone: "(305)-375-0300",
@@ -40,6 +41,7 @@ const VENDOR_DATA = [{
     name: "Tria's Flowers",
     short_description: "Trias Flowers has been recognized and awarded #1 in City Search Miami and Coral Gables Living for five consecutive years and \"Top 200\" florists in the country for the last five years by Teleflora. ",
     description: "Trias Flowers & Events is a full service floral wedding and event planning company. A fourth generation family-owned business, it is one of the highest regarded florists in South Florida. With over 100 years of experience, Trias Flowers & Events has a reputation for award-winning flowers, memorable weddings & events with warm, friendly service.",
+    image_url: "Florist_3.jpg",
     contact: {
       address: "6520 Bird Road Miami, FL 33155",
       phone: "(305)-749-0314 ",
@@ -57,6 +59,7 @@ const VENDOR_DATA = [{
     name: "Anthology Floristry & Event Design",
     short_description: "Anthology Co. is a floral studio and styling boutique for social and corporate events. Behind the company stands a strong philosophy to render the beauty of nature, admiration to artful design and history while mastering the art of setting the table.",
     description: "Anthology Co. is a floral studio and styling boutique for social and corporate events. Behind the company stands a strong philosophy to render the beauty of nature, admiration to artful design and history while mastering the art of setting the table. We are passionate about design, from the smallest detail to the grandest scheme, and we believe that styling flowers & events is an exciting, shared creative process with each client.",
+    image_url: "Florist_4.jpg",
     contact: {
       address: "6951 NW 16th St, Miami, FL 33126",
       phone: "(305)-988-6612",
@@ -75,6 +78,7 @@ const VENDOR_DATA = [{
     name: "Unearthead Rentals",
     short_description: "We conceptualized the idea of Unearthed Vintage in 2011, bringing our ideas to life in January of 2012 as South Florida’s first vintage rental company, leading the way in the event industry with our carefully curated collection.",
     description: "We conceptualized the idea of Unearthed Vintage in 2011, bringing our ideas to life in January of 2012 as South Florida’s first vintage rental company, leading the way in the event industry with our carefully curated collection. With a passion to remain the largest vintage rental company in the southeast, we are dedicated to continuously evolving by incorporating new trends with timeless classics. We believe in quality, both in the rentals we offer and the service we provide, building meaningful relationships with everyone we come in contact with. We exist to create memorable experiences through our unique style and innovation.",
+    image_url: "Decor_1.jpg",
     contact: {
       address: "6951 nw 16th street, miami, Fl, 33126",
       phone: "(305)987-6946",
@@ -93,6 +97,7 @@ const VENDOR_DATA = [{
     name: "Event Decor South Florida",
     short_description: "Event Decor South Florida was established by combining experienced lighting and draping professionals. With the combined experience of both groups of professionals we have one of the fastest turn key operations in the event industry, largest inventory in flame retardant fabrics and lighting fixtures.",
     description: "Event Decor South Florida has multiple vehicles and a full staff of trained and experience event specialist. To ensure the best quality control we go through a rigorous training program for safety and proper installs and dismantles. We take a lot of pride of having the right licenses , insurance , and workers compensation. All of our drapes are flame certified which has met the US Flame Retardant Regulations. ",
+    image_url: "Decor_2.jpg",
     contact: {
       address: "6951 nw 16th street, miami, Fl, 33126",
       phone: "(305)987-6946",
@@ -110,6 +115,7 @@ const VENDOR_DATA = [{
     name: "Delux Event Decor",
     short_description: "DELUX EVENTS DECOR has 12 years of experience specializing in weddings and social events. Our team is a group of experienced designers, that are not only passionate about flowers but also about making dreams come true.",
     description: " DELUX EVENTS DECOR has 12 years of experience specializing in weddings and social events. Our team is a group of experienced designers, that are not only passionate about flowers but also about making dreams come true. We strive to make the extraordinary from the ordinary, we only have one goal: your complete satisfaction. Your wedding day will be one of the most exciting days of your life. We would love to work with you to plan the wedding of your dreams. Our specialized team will work closely with you to create a unique and personalized design that will reflect you!",
+    image_url: "Decor_3.jpg",
     contact: {
       address: "12020 SW 132CT MIAMI FL, 33186",
       phone: "(305)-238-5733",
@@ -127,6 +133,7 @@ const VENDOR_DATA = [{
     name: "All Event Decoration",
     short_description: "At All Event Decoration & Wedding Rentals, we understand your need to have everything perfect on your big occasion. We offer a wide variety of party equipment and furniture on rent. ",
     description: "At All Event Decoration & Party Rental, we understand your need to have everything perfect on your big occasion. We offer a wide variety of party equipment and furniture on rent.",
+    image_url: "Decor_4.jpg",
     contact: {
       address: "2639 W 3rd Ct Hialeah FL 33010",
       phone: "(305) 305-1852",
@@ -145,6 +152,7 @@ const VENDOR_DATA = [{
     name: "The Bath Club",
     short_description: "The Bath Club is a hidden gem located on the beach in the beautiful City of Miami Beach and is is the ideal setting for a romantic and memorable wedding.",
     description: "The Bath Club is a hidden gem located on the beach in the beautiful City of Miami Beach and is is the ideal setting for a romantic and memorable wedding. With over 26,000 square feet of event space, your guests will experience a different era as they enter our unique and charismatic rooms. From a simple yet distinctive cocktail to an 8-course sit-down meal, The Bath Club offers the ideal rental venue for an unforgettable event.",
+    image_url: "Bath_club.jpg",
     contact: {
       address: "5937 Collins Avenue, Miami Beach, FL 33140",
       phone: "(305)-662-4742",
@@ -162,6 +170,7 @@ const VENDOR_DATA = [{
     name: "Villa Woodbine",
     short_description: "You can be assured that we will help you create an event that will bring rave reviews from your guests. ",
     description: "Villa Woodbine is a private mansion located in Coconut Grove,just south of downtown Miami on South Bayshore Drive.Exclusive to Bill Hansen Catering,the historic property is a treasure of a bygone era.",
+    image_url: "Villa_Woodbine.jpg",
     contact: {
       address: "2167 South Bayshore Drive Miami, Florida 33133",
       phone: "(305)-858-6660",
@@ -179,6 +188,7 @@ const VENDOR_DATA = [{
     name: "Comber Hall",
     short_description: "Comber Hall is a beautiful two story venue located next to Church of the Little Flower in the heart of Coral Gables with textured walls, original ceiling paintings and exposed wood beams in the downstairs lobby and upstairs ballroom.",
     description: " The venue is incredibly spacious, seating 300 guests in the downstairs ballroom with a dance floor, including a stage for entertainment and the lower front lobby for a cocktail hour. The lobby is complete with a beautiful entry table perfect for escort cards, artistic mirror details and complementing soft seating. The upstairs ballroom seats 250 with a dance floor, including a stage for entertainment and the upstairs lobby for a cocktail hour. The space features terraces overlooking Coral Gables, the church and surrounding grounds on both sides, and all glass doors offer gorgeous sunlight exposure for daytime events.",
+    image_url: "Comber_hall.jpg",
     contact: {
       address: "2711 Indian Mound Trail, Coral Gables, FL 33134",
       phone: "(786)-614-9908",
@@ -196,6 +206,7 @@ const VENDOR_DATA = [{
     name: "Thalatta Estate",
     short_description: "With its historic significance in South Florida and stunning bayside location, Thalatta Estate is a unique setting for your outdoor wedding ceremony, reception, or special event.",
     description: "Thalatta Estate is a unique setting for your outdoor wedding ceremony and reception. This magnificent site offers many unique amenities for up to 225 guests, including a waterside ceremony area, waterfalls cascading into a romantic reflection pond, manicured gardens, covered terrace, bridal suite, and catering kitchen.",
+    image_url: "Thalatta_estate.jpg",
     contact: {
       address: "17301 Old Cutler Rd, Palmetto Bay, FL 33157",
       phone: "(305)-662-4742",
@@ -214,6 +225,7 @@ const VENDOR_DATA = [{
     name: "Christian Arevalo",
     short_description: "My camera is a tool to freeze moments in time. Moments shared  with your family, friends, or even strangers. These are moments we can never get back, and the only way to relive them is to be able to see them in front of you.",
     description: "My camera is a tool to freeze moments in time. Moments shared  with your family, friends, or even strangers. These are moments we can never get back, and the only way to relive them is to be able to see them in front of you, printed on paper or seen on a screen.  What makes me tick is having the ability to supply that frozen moment to everyone I can. If you can relive a moment through a photograph, what else can you ask for?",
+    image_url: "Photography_1.jpg",
     contact: {
       address: "191 SW 12th Street, Miami, Fl, 33130",
       phone: "(786)-614-9908",
@@ -231,6 +243,7 @@ const VENDOR_DATA = [{
     name: "Alan Phillip Photography",
     short_description: "We are Alan and Raul and we are obsessed with capturing love! We were named one of Miami’s best wedding photographers.",
     description: " In our experience, the best method in capturing life's true moments is through a candid and relaxed style of photographing, which allows people to feel comfortable and reveal their true selves. Allow us the opportunity to capture your moments.",
+    image_url: "Photography_2.jpg",
     contact: {
       address: "CORAL GABLES, FL",
       phone: "305-281-7711",
@@ -250,6 +263,7 @@ const VENDOR_DATA = [{
     name: "Aaron's Catering",
     short_description: "Aaron’s Catering is a food driven, farm to table, catering company for over 12 years providing innovative and out of the box presentations with a twist. ",
     description: "We focus on setting the trends by using innovation, tight relationships with local farmers, technology, culinary alliances and being heavily involved with the industry. Chef Brian Aaron brings food, art and science together to create an unbelievable experience along with an impeccable service, out of the box presentations and eye-candy staff.",
+    image_url: "catering-1.jpg",
     contact: {
       address: "2517 NW 21st Terrace #11, Miami, FL 33142",
       phone: "(305) 513-4760",
@@ -265,8 +279,9 @@ const VENDOR_DATA = [{
     id: 8,
     category_id: 5,
     name: "Joy Wallace",
-    short_description: "",
-    description: "",
+    short_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+    image_url: "catering-2.jpg",
     contact: {
       address: "8501 SW 129th Terrace, Miami, FL 33156",
       phone: "(305) 252-0020",
@@ -285,6 +300,7 @@ const VENDOR_DATA = [{
     name: "Biscayne String Quartet",
     short_description: "The Biscayne String Quartet is one of the most sought after Miami based ensembles for wedding ceremony music, corporate events, and parties.  ",
     description: "The Biscayne String Quartet is one of the most sought after Miami based ensembles for wedding ceremony music, corporate events, and parties.  The group performs both classical music as well as original arrangements of popular non-classical songs, bringing a level of artistry and professionalism that your guests are sure to notice.",
+    image_url: "Music_1.jpg",
     contact: {
       address: "191 SW 12th Street,Miami,Fl,33130",
       phone: "(786)614-9908",
@@ -301,7 +317,8 @@ const VENDOR_DATA = [{
     category_id: 6,
     name: "Innocenti Strings",
     short_description: "Here we will provide the most talented string players in your area for just about any events. It has been over ten years now and we are so happy to be able to help clients, brides and grooms all over the country.",
-    description: "",
+    description: "Here we will provide the most talented string players in your area for just about any events. It has been over ten years now and we are so happy to be able to help clients, brides and grooms all over the country. Here we will provide the most talented string players in your area for just about any events. It has been over ten years now and we are so happy to be able to help clients, brides and grooms all over the country.",
+    image_url: "Music_2.jpg",
     contact: {
       address: "",
       phone: "",
@@ -309,8 +326,8 @@ const VENDOR_DATA = [{
     },
     voucher: {
       title: "",
-      value: 0,
-      price: 0
+      value: 2500,
+      price: 1675
     }
   }
 ]
